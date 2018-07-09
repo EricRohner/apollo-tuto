5d76c9488fb95f39b381ad04bfefcae26c1e4249
Eric Rohner <ericrohner22@gmail.com>
2018-07-08 19:17:07 -0600

    first commit

---
+++ src/components/LinkList.js
@@ -0,0 +1,66 @@
+import React, { Component, Fragment } from 'react'
+import Link from './Link'
+import { graphql } from 'react-apollo'
+import gql from 'graphql-tag'
+
+class LinkList extends Component {
+    render() {
+        // 1
+        if (this.props.feedQuery && this.props.feedQuery.loading) {
+            return <Fragment>Loading</Fragment>
+        }
+
+        // 2
+        if (this.props.feedQuery && this.props.feedQuery.error) {
+            return <Fragment>Error</Fragment>
+        }
+
+        // 3
+        const linksToRender = this.props.feedQuery.feed.links
+
+        return (
+            <div>
+                {linksToRender.map((link, index) => (
+                    <Link key={link.id} updateStoreAfterVote={this._updateCacheAfterVote} index={index} link={link}/>
+                ))}
+            </div>
+        )
+    }
+
+    _updateCacheAfterVote = (store, createVote, linkId) => {
+        // 1
+        const data = store.readQuery({ query: FEED_QUERY })
+
+        // 2
+        const votedLink = data.feed.links.find(link => link.id === linkId)
+        votedLink.votes = createVote.link.votes
+
+        // 3
+        store.writeQuery({ query: FEED_QUERY, data })
+    }
+}
+
+export const FEED_QUERY = gql`
+    query FeedQuery {
+        feed {
+            links {
+                id
+                createdAt
+                url
+                description
+                postedBy {
+                    id
+                    name
+                }
+                votes {
+                    id
+                    user {
+                        id
+                    }
+                }
+            }
+        }
+    }
+`
+
+export default graphql(FEED_QUERY, { name: 'feedQuery' }) (LinkList)
